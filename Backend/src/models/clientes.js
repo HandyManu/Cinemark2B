c52aade853de9297d8748bdba8d5f5317881ca9b
Manuel <20220416@ricaldone.edu.sv>
2025-05-13 09:06:21 -0600

    modelo 3 terminado

---
+++ Backend/src/models/clientes.js
@@ -0,0 +1,30 @@
+import { Schema,model } from "mongoose";
+
+const clientesSchema = new Schema({
+
+    nombre: {
+        type: String,
+        required: true
+      },
+      correo: {
+        type: String,
+        required: true
+      },
+      telefono: {
+        type: String,
+        required: true
+      },
+      direccion: {
+        type: String,
+        required: true
+      },
+      activo: {
+        type: Boolean,
+        required: true
+      }
+},{
+    timestamps:true,
+    strict:false
+})
+
+export default model("clientes",clientesSchema );
